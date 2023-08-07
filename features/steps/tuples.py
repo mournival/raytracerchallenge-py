8d033b3b7b2591463c23c55449161ea08c2e55a0
kholvoet <kwh@mournival.com>
2023-08-07 12:22:19 -0400

    Implement code for tuple.features test #4

--- features/steps/tuples.py
+++ features/steps/tuples.py
@@ -1,6 +1,6 @@
 from behave import *
 
-from features.tuple import Tuple, point
+from features.tuple import Tuple, point, vector
 
 use_step_matcher("parse")
 
@@ -89,3 +89,15 @@ def step_impl(context, name, x, y, z, w):
     :type context: behave.runner.Context
     """
     assert (context.tuples[name] == Tuple(x, y, z, w))
+
+
+@given("{} ← vector({:g}, {:g}, {:g})")
+def step_impl(context, name, x, y, z):
+    """
+    :param name: Point Name
+    :param x:
+    :param y:
+    :param z:
+    :param w:
+    :type context: behave.runner.Context    """
+    context.tuples[name] = vector(x, y, z)
--- features/tuple.py
+++ features/tuple.py
@@ -1,10 +1,10 @@
 from collections import namedtuple
 
-class Tuple(namedtuple('Tuple',[('x'), ('y'), ('z'), ('w')])):
+
+class Tuple(namedtuple('Tuple', [('x'), ('y'), ('z'), ('w')])):
 
     def isPoint(self) -> bool:
         return self.w == 1.0
-
 
     def isVector(self) -> bool:
         return self.w == 0.0
@@ -12,3 +12,7 @@ class Tuple(namedtuple('Tuple',[('x'), ('y'), ('z'), ('w')])):
 
 def point(x: float, y: float, z: float) -> Tuple:
     return Tuple(x, y, z, 1.0)
+
+
+def vector(x: float, y: float, z: float) -> Tuple:
+    return Tuple(x, y, z, 0.0)
