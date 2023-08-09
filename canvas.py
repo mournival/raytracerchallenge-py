9bd09b924d1613a8ea8c9c857b18a2f7a3da9e11
kholvoet <kwh@mournival.com>
2023-08-09 07:19:01 -0400

    Tiny refactor in canvas.py: use pythonic var init / declaration.

--- canvas.py
+++ canvas.py
@@ -10,8 +10,7 @@ class Canvas(object):
         for r in range(width):
             for c in range(height):
                 self._data[r, c] = Color(0, 0, 0)
-        self.width = self._data.shape[0]
-        self.height = self._data.shape[1]
+        self.width, self.height = self._data.shape
 
     def pixels(self):
         return list(self._data.flatten())
