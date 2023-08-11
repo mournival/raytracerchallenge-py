723b8b4b782cfaacff81e8a19b2657533a8824ec
kholvoet <kwh@mournival.com>
2023-08-11 13:24:34 -0400

    Reorder matrix.py contents.

--- matrix.py
+++ matrix.py
@@ -1,28 +1,29 @@
 import numpy as np
 
 
+def matrix(table_data):
+    return np.array(table_data, dtype='float')
+
+
 def eye(n):
     return np.eye(n)
+
+
+def array_equal(A, B):
+    return np.array_equal(A, B)
 
 
 def det(A):
     return np.linalg.det(A)
 
 
-def matrix(table_data):
-    return np.array(table_data, dtype='float')
-
-
-def transpose(A):
-    return np.transpose(A)
+def dot(a, b):
+    return np.dot(a, b)
 
 
 def matmul(A, B):
     return np.matmul(A, B)
 
-def dot(a, b):
-    return np.dot(a, b)
 
-def array_equal(A, B):
-    return np.array_equal(A, B)
-
+def transpose(A):
+    return np.transpose(A)
