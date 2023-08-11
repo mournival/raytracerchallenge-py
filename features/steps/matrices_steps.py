5dda58b0c96df0ba6cf086127d43af8cae9cf8be
kholvoet <kwh@mournival.com>
2023-08-11 09:35:22 -0400

    Enable more matrix creation tests. Implement steps for matrix equality.

--- features/steps/matrices_steps.py
+++ features/steps/matrices_steps.py
@@ -14,7 +14,21 @@ def step_impl(context, m , n, name):
     table_data.insert(0, heading_row)
     context.globals[name] = np.array(table_data, dtype='float')
 
+@given("the following matrix {:w}")
+def step_impl(context, name):
+    heading_row = Row(context.table.headings, context.table.headings)
+    table_data = context.table.rows
+    table_data.insert(0, heading_row)
+    context.globals[name] = np.array(table_data, dtype='float')
 
 @then("{:w}[{:d},{:d}] = {:g}")
 def step_impl(context, name, x, y, expected):
     assert_equal(context.globals[name][x, y], expected)
+
+
+@then("{:l} = {:l}")
+def step_impl(context, a, b):
+    assert_array_equal(context.globals[a], context.globals[b])
+
+def assert_array_equal(actual, expected):
+        assert np.array_equal(actual, expected), f"{actual} != {expected}"
