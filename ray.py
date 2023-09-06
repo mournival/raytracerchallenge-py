c01eee43b33eb121dda4ee5a085e64c8fe0b196b
kholvoet <kwh@mournival.com>
2023-09-06 08:15:31 -0400

    Implement lighting function and enable rest of tests for chapter 6.

--- ray.py
+++ ray.py
@@ -1,7 +1,10 @@
+import math
 from collections import namedtuple
 
+import tuple
 from color import color
 from matrix import dot
+from tuple import normalize
 
 ray = namedtuple('Ray', 'origin direction')
 
@@ -26,5 +29,25 @@ material = namedtuple('Material',
                       ))
 
 
-def lighting(m, light, position, eyev, normalv):
-    return color(1.9, 1.9, 1.9)
+def lighting(m, light, pos, eye_v, normal_v):
+    effective_color = m.color * light.intensity
+    light_v = normalize(light.position - pos)
+
+    ambient = effective_color * m.ambient
+    light_dot_normal = dot(light_v, normal_v)
+    if light_dot_normal < 0:
+        diffuse = color(0, 0, 0)
+        specular = color(0, 0, 0)
+    else:
+        diffuse = effective_color * light_dot_normal * m.diffuse
+
+        reflect_v = tuple.reflect(-light_v, normal_v)
+        reflect_dot_eye = dot(reflect_v, eye_v)
+
+        if reflect_dot_eye <= 0:
+            specular = color(0, 0, 0)
+        else:
+            factor = math.pow(reflect_dot_eye, m.shininess)
+            specular = light.intensity * m.specular * factor
+
+    return ambient + diffuse + specular
