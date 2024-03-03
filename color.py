Fix bug in ppm generation (off by one). Fix line length in ppm file generation. Fix Refactoring induced errors in older putting it all together files.

color.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def color(r: float = 1.0, g: float= 1.0, b: float= 1.0):
+def color(r: float = 1.0, g: float = 1.0, b: float = 1.0):
     return np.array([r, g, b])
 
 
@@ -17,19 +17,17 @@ def blue(c):
     return c[2]
 
 
-def _clamp(c: float, min_val, max_val):
-    v = round(c * max_val)
-    if v < min_val:
-        return str(min_val)
-    if v > max_val:
-        return str(max_val)
+def _clamp(c: float):
+    v = round(c * 255)
+    if v < 0:
+        return "0"
+    if v > 255:
+        return "255"
     return str(v)
 
 
-def clamp_color(c, min_val, max_val):
-    return [_clamp(c[0], min_val, max_val),
-            _clamp(c[1], min_val, max_val),
-            _clamp(c[2], min_val, max_val)]
+def clamp_color(c):
+    return [_clamp(c[0]), _clamp(c[1]), _clamp(c[2])]
 
 
 BLACK = color(0, 0, 0)

putting_it_together/chapter7.py
@@ -13,7 +13,7 @@ from tuple import point, vector3
 from world import World
 
 
-def main():
+def main():  # pragma: no cover
     floor = sphere(scaling(10, 0.01, 10), material(color=color(1, 0.9, 0.9), specular=0))
     left_wall = sphere(np.matmul(matrix.translation(0, 0, 5), np.matmul(matrix.rotation_y(- math.pi / 4),
                                                                         np.matmul(matrix.rotation_x(math.pi / 2),
@@ -40,7 +40,7 @@ def main():
             f.write(line)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     start = datetime.datetime.now()
     main()
     print(f"{datetime.datetime.now() - start}")