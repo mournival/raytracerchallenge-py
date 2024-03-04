049e1efe6fc4990b9015bb5a4b3fe24ad8b6dc15
kholvoet <kwh@mournival.com>
2024-03-04 16:31:11 -0500

    Refactor putting it togeter chapter8 and chapter9.

--- putting_it_together/chapter7.py
+++ putting_it_together/chapter7.py
@@ -13,7 +13,7 @@ from tuple import point, vector3
 from world import World
 
 
-def main():  # pragma: no cover
+def create_image():
     floor = sphere(scaling(10, 0.01, 10), material(color=color(1, 0.9, 0.9), specular=0))
     left_wall = sphere(np.matmul(matrix.translation(0, 0, 5), np.matmul(matrix.rotation_y(- math.pi / 4),
                                                                         np.matmul(matrix.rotation_x(math.pi / 2),
@@ -28,12 +28,14 @@ def main():  # pragma: no cover
                    material(color=color(0.5, 1, 0.1), diffuse=0.7, specular=0.3))
     left = sphere(np.matmul(matrix.translation(-1.5, 0.33, -0.75), scaling(0.33, 0.33, 0.33)),
                   material(color=color(1.0, 0.8, 0.1), diffuse=0.7, specular=0.3))
-
     w = World(point_light(point(-10, 10, -10), color(1, 1.1)), [floor, left_wall, right_wall, middle, left, right])
-
     c = Camera(1000, 500, math.pi / 3, matrix.view_transform(point(0, 1.5, -5), point(0, 1, 0), vector3(0, 1, 0)))
-
     image = c.render(w)
+    return image
+
+
+def main():  # pragma: no cover
+    image = create_image()
 
     with open('../images/chapter7.ppm', 'w', encoding="ascii") as f:
         for line in image.to_ppm():
